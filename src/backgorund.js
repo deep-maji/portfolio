399538f92e04076674d75d8e06cb60522b5e5f65
deep-maji <majideep179@gmail.com>
2025-07-18 19:30:22 +0530

    html css

--- src/backgorund.js
+++ src/backgorund.js
@@ -3,7 +3,7 @@ const bg = new FinisherHeader({
     "size": {
         "min": 2,
         "max": 3,
-        "pulse": 0
+        "pulse": 0.01
     },
     "speed": {
         "x": {
@@ -12,7 +12,7 @@ const bg = new FinisherHeader({
         },
         "y": {
             "min": 0,
-            "max": 0.1
+            "max": 0.2
         }
     },
     "colors": {
@@ -23,7 +23,7 @@ const bg = new FinisherHeader({
     },
     "blending": "lighten",
     "opacity": {
-        "center": 0.6,
+        "center": 1,
         "edge": 0
     },
     "skew": 0,
@@ -33,3 +33,45 @@ const bg = new FinisherHeader({
         "t"  // triangle
     ]
 });
+
+
+let main = document.querySelector("main");
+window.addEventListener("resize", (event) => {
+    let canvas = document.querySelector("canvas");
+    canvas.remove();
+    new FinisherHeader({
+    "count": 2000,
+    "size": {
+        "min": 2,
+        "max": 3,
+        "pulse": 0.01
+    },
+    "speed": {
+        "x": {
+            "min": 0,
+            "max": 0.2
+        },
+        "y": {
+            "min": 0,
+            "max": 0.2
+        }
+    },
+    "colors": {
+        "background": "#000000",
+        "particles": [
+            "#ffffff"
+        ]
+    },
+    "blending": "lighten",
+    "opacity": {
+        "center": 1,
+        "edge": 0
+    },
+    "skew": 0,
+    "shapes": [
+        "c", // circle
+        "s", // square
+        "t"  // triangle
+    ]
+});
+})
--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
 
-// document.addEventListener('DOMContentLoaded', () => {
-//     const scroll = new LocomotiveScroll({
-//         el: document.querySelector('main'),
-//         smooth: true
-//     });
-// });
+document.addEventListener('DOMContentLoaded', () => {
+    const scroll = new LocomotiveScroll({
+        el: document.querySelector('main'),
+        smooth: true
+    });
+});
 
 
-let main = document.querySelector("main");
-window.addEventListener("resize", (event) => {
-    window.location.reload();
-})
+// let main = document.querySelector("main");
+// window.addEventListener("resize", (event) => {
+//     window.location.reload();
+// })
+
+let loader = document.querySelector("#loader");
+setTimeout(()=>{
+    loader.style.top = "-200%"
+},3000)
 
